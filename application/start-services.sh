52944be36a84e147180e1a1f078fecd89925957a
CatalinFetoiu <fetoiucatalinemil@gmail.com>
2019-05-07 13:00:37 -0700

    Add scripts to start and stop application

---
+++ application/start-services.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo docker-compose pull
+
+sudo docker-compose run -d log_generator
+sudo docker-compose run -d --name rabbitmq_service rabbitmq_service
+sudo docker-compose run -d rabbit_worker
+sudo docker-compose run -d --name elasticsearch_service elasticsearch_service
+sudo docker-compose run -T client
