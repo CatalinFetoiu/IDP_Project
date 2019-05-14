92b36483d736ed2a06446c3af2f14c86b7897e56
fetoiucatalinemil@gmail.com <fetoiucatalinemil@gmail.com>
2019-05-14 14:21:35 +0000

    Add docker exec command to start-services script

--- application/start-services.sh
+++ application/start-services.sh
@@ -6,4 +6,6 @@ sudo docker-compose run -d log_generator
 sudo docker-compose run -d --name rabbitmq_service rabbitmq_service
 sudo docker-compose run -d rabbit_worker
 sudo docker-compose run -d --name elasticsearch_service elasticsearch_service
-sudo docker-compose run -T client
+sudo docker-compose run -T --name client_container client
+
+sudo docker exec -it client_container /bin/bash
